f435d2e1fe29682ffb5c93202218e43af1f8f734
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2025-10-23 11:41:20 +0530

    fix code to as per req

--- lab9/UDP_Client.java
+++ lab9/UDP_Client.java
@@ -5,9 +5,10 @@ class UDP_Client {
         DatagramSocket skt = new DatagramSocket(9999); 
         byte[] buffer = new byte[1024];
         System.out.println("Client listening on port 9999...");
-        DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-        skt.receive(reply);
-        System.out.println("Client received: " + new String(reply.getData()));
-        skt.close();
+        while (true) {
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            skt.receive(reply);
+            System.out.println("Client received: " + new String(reply.getData()));
+        }
     }
 }
